42593d3f298f69d9e4f2d560e367cd53afbaa62e
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-03-19 12:20:17 +0100

    solution

--- src/main/scala/QueenAttack.scala
+++ src/main/scala/QueenAttack.scala
@@ -1,12 +1,10 @@
 case class Queen(x: Int, y: Int)
 
-object Queen {
+object Queen:
    def create(row: Int, col: Int): Option[Queen] =
-      if (row < 0 || col < 0 || row > 7 || col > 7) None
+      if row < 0 || col < 0 || row > 7 || col > 7 then None
       else Some(Queen(row, col))
-}
 
-object QueenAttack {
+object QueenAttack:
    def canAttack(q1: Queen, q2: Queen): Boolean =
       q1.x == q2.x || q1.y == q2.y || (q1.x - q2.x).abs == (q1.y - q2.y).abs
-}
